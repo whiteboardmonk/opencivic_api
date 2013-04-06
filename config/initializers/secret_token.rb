5aa33c2f28a043bf9b971ea5ab296875b6c01164
Akshay Surve <akshay.surve@gmail.com>
2013-04-06 15:05:11 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpencivicApi::Application.config.secret_token = 'd4c687e77171a9c505fa150ef386e4bc00baf8304e6ad7d16793b3270397ac543e2352374a1e713b211a92deabfb783c9e7ef4de34ced82fff9068b7c779bd12'
